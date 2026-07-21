0e888c83f126fb7152f7a1ecad2408ae83606c8b
Stratus <e.axarlis@hotmail.co.uk>
2026-07-21 11:56:32 +0300

    validators for commands categories

---
+++ CSharpApp.Application/Categories/Validators/DeleteCategoryValidator.cs
@@ -0,0 +1,14 @@
+﻿using CSharpApp.Application.Categories.Commands;
+using FluentValidation;
+
+namespace CSharpApp.Application.Categories.Validators;
+
+
+public sealed class DeleteCategoryValidator : AbstractValidator<DeleteCategoryCommand>
+{
+    public DeleteCategoryValidator()
+    {
+
+        RuleFor(v => v.Id).GreaterThan(0);
+    }
+}
---
+++ CSharpApp.Application/Categories/Validators/UpdateCategoryValidator.cs
@@ -0,0 +1,16 @@
+﻿using CSharpApp.Application.Categories.Commands;
+using FluentValidation;
+
+namespace CSharpApp.Application.Categories.Validators;
+
+
+public sealed class UpdateCategoryValidator : AbstractValidator<UpdateCategoryCommand>
+{
+    public UpdateCategoryValidator()
+    {
+
+        RuleFor(v => v.Dto.Name).NotEmpty();
+        RuleFor(v => v.Id).GreaterThan(0);
+        RuleFor(v => v.Dto.Name).MaximumLength(100);
+    }
+}
